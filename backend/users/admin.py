a1178814d2190a8d32f8bdd2843b8e7432403405
Никита Ишмекеев <nikita.ishmekeev.2003@gmail.com>
2023-09-11 19:31:08 +0300

    добавил модели пользователя и подписки

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Follow
+
+class UserAdmin(admin.ModelAdmin):
+    """Управление пользователем"""
+
+    list_display = ('id', 'username', 'firt_name', 'last_name',
+                    'email')
+    list_display_links = ('id', 'username')
+    list_filter = ('email', 'username')
+
+
+class FollowAdmin(admin.ModelAdmin):
+    """Управление подписками на автора"""
+
+    list_display = ('id', 'user', 'following')
+    search_fields = ('user',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Follow, FollowAdmin)
